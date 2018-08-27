4923dd1e28c2f4276b4ccaea4dd3424937f7ac37
Krystal Williams <asterika@Krystals-MacBook-Pro.local>
2018-08-27 13:28:29 -0700

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,40 +1,68 @@
-// Create an array to store all cards
-var cards = ['queen', 'queen', 'king', 'king'];
+// Create an array to store all card objects
+var cards = [
+  {
+    card: 'queen',
+    suit: 'hearts',
+    cardImage: 'images/queen-of-hearts.png'
+  },
+  {
+    card: 'queen',
+    suit: 'diamonds',
+    cardImage: 'images/queen-of-diamonds.png'
+  },
+  {
+    card: 'king',
+    suit: 'hearts',
+    cardImage: 'images/king-of-hearts.png'
+  },
+  {
+    card: 'king',
+    suit: 'diamonds',
+    cardImage: 'images/king-of-diamonds.png'
+  }
+];
 
 // Create an array to store the cards in play
 var cardsInPlay = [];
 
-//Create a function to check for card match
+// Create a function to check for a match
 var checkForMatch = function () {
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-		console.log("You found a match!");
-		} else {
-		console.log("Sorry, try again.");	
-		}
+  // Check to see if two cards match and provide feedback to user
+  if (cardsInPlay[0] === cardsInPlay[1]) {
+    console.log("You found a match!");
+  } else {
+    console.log("Sorry, try again.");
+  }
 };
 
-//Create a function to represnt a user flipping a card
+// Create a function to represent a user flipping a card
 var flipCard = function (cardId) {
-	//Call the flipcard function
-	flipCard(0);
-	flipCard(2);
-	//Display the card the user just flipped	
-	console.log("User flipped " + cards[cardId]);
 
-	//Add card to array of cards in play
-	cardsInPlay.push(cards[cardId]);
+  // Display the card the user just flipped in the console.
+  console.log("User flipped " + cards[cardId].card);
 
+  // Add card to array of cards that are in play
+  cardsInPlay.push(cards[cardId].card);
 
-//Check to see if two cards have been played
-if (cardsInPlay.length === 2) {
-	//If so, call the checkForMatch function
-	checkForMatch;
-}
+  // Display the image path in the console
+  console.log(cards[cardId].cardImage)
+
+  // Display the suit in the console
+  console.log(cards[cardId].suit)
+
+  // Check to see if two cards have been played
+  if (cardsInPlay.length === 2) {
+    // If so, call the checkForMatch function
+    checkForMatch();
+    // Empty cards in play array for next try
+    cardsInPlay = [];
+  }
 };
 
-//Check to see if two cards match and provide feedback to the user
- if (cardsInPlay[0] === cardsInPlay[1]) { 
-	alert("You found a match!");
- } else {
-	alert("Sorry, try again.");
- };
+// For now, we are manually calling the flipCard function
+// to represent a user's play
+// Call the flipCard function, passing in an index as the argument
+flipCard(0);
+// Call the flipCard function, passing in an index as the argument
+flipCard(2);
+
