b2ee48756aeff2e2c7b069a8cd31be21f3ee26ed
Krystal Williams <asterika@Krystals-MacBook-Pro.local>
2018-08-28 15:28:47 -0700

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,68 +1,86 @@
-// Create an array to store all card objects
+//Create an array to store all card objects
 var cards = [
   {
-    card: 'queen',
+    rank: 'queen',
     suit: 'hearts',
-    cardImage: 'images/queen-of-hearts.png'
+    cardImage: 'images/queen-of-hearts.png',
   },
   {
-    card: 'queen',
+    rank: 'queen',
     suit: 'diamonds',
-    cardImage: 'images/queen-of-diamonds.png'
+    cardImage: 'images/queen-of-diamonds.png',
   },
   {
-    card: 'king',
+    rank: 'king',
     suit: 'hearts',
-    cardImage: 'images/king-of-hearts.png'
+    cardImage: 'images/king-of-hearts.png',
   },
   {
-    card: 'king',
+    rank: 'king',
     suit: 'diamonds',
-    cardImage: 'images/king-of-diamonds.png'
-  }
+    cardImage: 'images/king-of-diamonds.png',
+  },
 ];
 
 // Create an array to store the cards in play
 var cardsInPlay = [];
 
 // Create a function to check for a match
-var checkForMatch = function () {
+function checkForMatch() {
+
   // Check to see if two cards match and provide feedback to user
+  //if (cardsInPlay.length === 2) {
   if (cardsInPlay[0] === cardsInPlay[1]) {
-    console.log("You found a match!");
+    alert("You found a match!");
   } else {
-    console.log("Sorry, try again.");
-  }
+    alert("Sorry, try again.");
+  };
 };
 
 // Create a function to represent a user flipping a card
-var flipCard = function (cardId) {
+function flipCard() {
 
-  // Display the card the user just flipped in the console.
-  console.log("User flipped " + cards[cardId].card);
+  var cardId = this.getAttribute('data-id');
 
-  // Add card to array of cards that are in play
-  cardsInPlay.push(cards[cardId].card);
-
-  // Display the image path in the console
-  console.log(cards[cardId].cardImage)
-
-  // Display the suit in the console
-  console.log(cards[cardId].suit)
+//Add card to array of cards in play
+  cardsInPlay.push(cards[cardId].rank);
+  this.setAttribute('src', cards[cardId].cardImage);
 
   // Check to see if two cards have been played
   if (cardsInPlay.length === 2) {
-    // If so, call the checkForMatch function
+
+    // If yes, call the checkForMatch function
     checkForMatch();
+
     // Empty cards in play array for next try
     cardsInPlay = [];
-  }
+  };
 };
 
-// For now, we are manually calling the flipCard function
-// to represent a user's play
-// Call the flipCard function, passing in an index as the argument
-flipCard(0);
-// Call the flipCard function, passing in an index as the argument
-flipCard(2);
+// Create a function to create board
+function createBoard() {
+
+  // Loop through cards array to create card elements
+  for (var i = 0; i < cards.length; i+) {
+
+    // Create an img element to be used as a card
+    var cardElement = document.createElement('img');
+
+    // Set src attribute to display back of card image
+    cardElement.setAttribute('src', 'images/back.png');
+
+    // Set card's 'data-id' attribute as index of current element
+    cardElement.setAttribute('data-id', i);
+
+    // Add an event listener so when a card is clicked, 
+    // function flipCard is executed
+    cardElement.addEventListener('click', flipCard);
+
+    // Add card to board
+    document.getElementById('game-board').appendChild(cardElement);
+  }
+}
+
+// Call createBoard function
+createBoard();
 
